c6f9f73c3efdb9774baf34b6946df52b42c77c51
Cliff Workman <cworkman@eborkdev.com>
2023-03-16 20:39:46 -0400

    Bring mdc attributes up a level in the log context

--- src/log/screeps-logger.ts
+++ src/log/screeps-logger.ts
@@ -3,12 +3,11 @@ const rawLog = console.log;
 let mdc: Record<string, string> = {};
 
 export const log = (message: any): void => {
-    rawLog(JSON.stringify({
-        mdc,
+    rawLog(JSON.stringify(Object.assign({}, mdc, {
         tick: Game.time,
         timestamp: new Date().toISOString(),
         message
-    }, null, 2));
+    }, null, 2)));
 }
 
 export const setMdcValue = (key: string, value: string): void => {
